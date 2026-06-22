e37a8fd02ee7ee23f8f8ad9718b6c5a232779f27
Arvind Venkatesh <git@rvind.cc>
2026-06-22 16:08:02 -0700

    feat: frontend layout

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from "express"
 const app = express();
 const port = 8080;
 
+app.use(express.static('public'))
+
 app.listen(port, () => {
   console.log(`Server @ http://localhost:${port}/`)
 })
